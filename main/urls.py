0f410bcc4f5cf240cc70c77ab8a1c9217d55fd83
Pavel <eco.kazachenPS@bsu.by>
2021-05-07 19:24:07 +0300

    add comment

--- main/urls.py
+++ main/urls.py
@@ -8,15 +8,4 @@ urlpatterns = [
 ]
 
 
-# echo "# myfirst_project" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/jabajke/myfirst_project.git
-# git push -u origin main
-# .idea /
-# db.sqlite3
-# main /
-# manage.py
-# webfirst /
+#asdasdasda
