d225f8cacdbb1bb52037302c68e06892748d4f5a
Pavel <aaamazafak@yandex.ru>
2021-08-12 16:45:45 +0300

    updated forms

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
@@ -14,6 +14,16 @@ class NewsDetailView(DetailView):
     template_name = 'news/detail.html'
     context_object_name = 'article'
 
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news_delete.html'
+    success_url = '/news'
 
 def create(request):
     error = ''
